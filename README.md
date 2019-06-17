a437ab83f87106618554edba81b4116e2859c53b
Jessica Z <jessica.jiajia.zhang@gmail.com>
2019-06-17 14:10:26 -0400

    requesting one and multiple products

--- README.md
+++ README.md
@@ -1,21 +1,21 @@
 # web-requests-exercise
 
-# Navigate to the repo via command-line
+## Navigate to the repo via command-line
 ```sh
 cd ~/Github/web-requests-exercise
 ```
 
-# Create and Activate a new Anaconda virtual environment
+## Create and Activate a new Anaconda virtual environment
 ```sh
 conda create -n requests-env python=3.7 # (first time only)
 conda activate requests-env
 ```
-# Install all necessary packages
+## Install all necessary packages
 ```sh
 pip install requests
 ```
 
-# When you are ready to run the program, execute the following from command-line
+## When you are ready to run the program, execute the following from command-line
 ```sh
 python get_data.py
 ```
--- get_data.py
+++ get_data.py
@@ -1,4 +1,37 @@
 # get_data.py
 
-print("REQUESTING SOME DATA FROM THE INTERNET...")
+import json, requests, pandas, html, os, urllib.request
 
+print("REQUESTING SOME DATA FROM THE INTERNET...")
+print("---------------------------------------------")
+## TODO: Requesting a Product
+request_url = "https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-201905/master/data/products/2.json"
+r = requests.get(request_url)
+print("URL1 has a single product:", r.json()["name"])
+# print(type(r.json()["name"]))
+
+# ALTERNATIVE PRINT METHOD
+    # with urllib.request.urlopen(request_url) as url:
+    #     data = json.loads(url.read().decode())
+    #     print(data)
+    #     print(type(data))
+
+
+## TODO: Requesting Products
+request_url_2 = "https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-201905/master/data/products.json"
+r2 = requests.get(request_url_2)
+prod_list = r2.json()
+print("---------------------------------------------")
+print("URL2 has the following products:")
+[print("NAME:", i["name"]+",", "ID:", str(i["id"])) for i in prod_list]
+print("---------------------------------------------")
+## TODO: Requesting the Gradebook
+request_url_3 = "https://raw.githubusercontent.com/prof-rossetti/nyu-info-2335-201905/master/data/gradebook.json"
+r3 = requests.get(request_url_3)
+grades = r3.json()
+print(type(grades))
+# with open(grades, "r") as grades_file:
+#     file_grades = grades_file.read()
+# gradebook = json.loads(file_grades)
+
+# print(gradebook)
